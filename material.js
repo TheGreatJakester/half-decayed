1e850cba8a4e722469b2e571863fb000259bea63
Jacob Moulton <Jacob.Moulton@gmail.com>
2018-10-23 12:42:33 -0600

    trying grid, doesn't work

--- material.js
+++ material.js
@@ -1,8 +1,44 @@
-
-function draw(){
-    console.log("draw()")
-    var materialWindow = document.getElementById("materialWindow");
-    var context = materialWindow.getContext("2d");
+function initMaterialWindow(){
+    materialWindow = document.getElementById("materialWindow");
+    width = materialWindow.width;
+    height = materialWindow.height;
+    context = materialWindow.getContext("2d");
     context.fillStyle = "#FF0000";
-    context.fillRect(0,0,200,200);
+    //infinite loop start
+    drawFrame(context)
+}
+
+
+
+length=50;
+samples = [];
+samples.lenght = lenght*length;
+
+
+function drawFrame(context){
+    var start = (new Date()).getTime();
+    //drawing code
+        for(i=0;i<length;i++){
+            for(j=0;j<length;i++){
+                if(samples[i*length+j] == 1){
+                    context.fillStyle = "#FF0000" //red
+                }
+                else{
+                    context.fillStyle = "#0000FF" //Green
+                }
+                console.log('trying to draw a rect')
+                context.fillRect(i*(width/length),j*(height/length),(width/length),(height/length))
+            }
+        }
+
+    //end drawing code
+    var finish = (new Date()).getTime();
+    var delta = finish - start;
+    var framesPerSecond = 30;
+    var waitTime = delta - 1000/framesPerSecond;
+    if(waitTime < 0){
+        waitTime = 0;
+    }
+    setTimeout(function(){drawFrame(context)},waitTime)
+    
 }
