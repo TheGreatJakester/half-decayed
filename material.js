29dcedd9fad0aad78753108765743319e80df2bf
Jacob Moulton <Jacob.Moulton@gmail.com>
2018-10-25 13:34:21 -0600

    simulation works
    There is an issue with randomly sorting the order though

--- material.js
+++ material.js
@@ -1,3 +1,36 @@
+LENGTH = 100;
+currentTime = 0;
+
+simulation = {
+    numberOfSamples : LENGTH*LENGTH,
+    order: new Array(this.numberOfSamples),
+    units: 'years',
+    halfLife : 100,
+    unitsPerSecond: 1,
+    getSamplesAt : function(seconds){
+        samples = new Array(this.numberOfSamples).fill(0)
+        numberOfDecayedSamples = 
+        Math.floor(
+            this.numberOfSamples*
+            Math.pow(
+                .5,
+                (seconds*this.unitsPerSecond)/this.halfLife
+            )
+        )
+        for(i=0;i<numberOfDecayedSamples;i++){
+            samples[this.order[i]] = 1;
+        }
+        return samples;
+    },
+    reset : function(){
+        //Make sure to run this atleast once before the first simulation goes
+        for(i=0;i<this.numberOfSamples;i++){
+            this.order[i] = i;
+        }
+        this.order.sort(Math.random)
+    }
+}
+
 function initMaterialWindow(){
     materialWindow = document.getElementById("materialWindow");
     width = materialWindow.width;
@@ -5,20 +38,14 @@ function initMaterialWindow(){
     context = materialWindow.getContext("2d");
     context.fillStyle = "#FF0000";
     //infinite loop start
+    simulation.reset()
     drawFrame(context)
 }
-
-
-
-LENGTH=150;
-samples = [];
-samples.length = LENGTH*LENGTH;
-
 
 function drawFrame(context){
     var start = (new Date()).getTime();
     //drawing code
-        samples[Math.floor(Math.random()*samples.length)] = 1;
+        samples = simulation.getSamplesAt(currentTime)
         for(i=0;i<LENGTH;i++){
             for(j=0;j<LENGTH;j++){
                 if(samples[i*LENGTH+j] == 1){
@@ -35,10 +62,10 @@ function drawFrame(context){
     var finish = (new Date()).getTime();
     var delta = finish - start;
     var framesPerSecond = 30;
+    currentTime += 1/framesPerSecond;
     var waitTime = 1000/framesPerSecond - delta;
     if(waitTime < 0){
         waitTime = 0;
     }
-    setTimeout(function(){drawFrame(context)},waitTime)
-    
+    setTimeout(function(){drawFrame(context)},waitTime)    
 }
